991c750c6ca46fdcfc0595326608f5c7464b1dda
omargonz01 <gomarzalez@gmail.com>
2024-01-19 16:01:03 -0800

    clean up & add animation.js

---
+++ animations.js
@@ -0,0 +1,28 @@
+
+function moveRight(gridRowIndex) {
+    // Get all of the cells in the current row
+    const currentRow = gridMatrix[gridRowIndex];
+  
+    // Remove the last element...
+    const lastElement = currentRow.pop();
+  
+    // And put it back to the beginning, i.e index 0
+    currentRow.unshift(lastElement);
+  }
+  
+  function moveLeft(gridRowIndex) {
+    const currentRow = gridMatrix[gridRowIndex];
+    const firstElement = currentRow.shift();
+    currentRow.push(firstElement);
+  }
+  
+  function animateGame() {
+    // Animate river:
+    moveRight(1);
+    moveLeft(2);
+  
+    // Animate road:
+    moveRight(4);
+    moveRight(5);
+    moveRight(6);
+  }
--- script.js
+++ script.js
@@ -99,33 +99,6 @@ function moveDuck(event) {
 // Animation Functions
 // can be in their own file, and add script to link in html
 
-function moveRight(gridRowIndex) {
-  // Get all of the cells in the current row
-  const currentRow = gridMatrix[gridRowIndex];
-
-  // Remove the last element...
-  const lastElement = currentRow.pop();
-
-  // And put it back to the beginning, i.e index 0
-  currentRow.unshift(lastElement);
-}
-
-function moveLeft(gridRowIndex) {
-  const currentRow = gridMatrix[gridRowIndex];
-  const firstElement = currentRow.shift();
-  currentRow.push(firstElement);
-}
-
-function animateGame() {
-  // Animate river:
-  moveRight(1);
-  moveLeft(2);
-
-  // Animate road:
-  moveRight(4);
-  moveRight(5);
-  moveRight(6);
-}
 
 function updateDuckPosition() {
   gridMatrix[duckPosition.y][duckPosition.x] = contentBeforeDuck;
